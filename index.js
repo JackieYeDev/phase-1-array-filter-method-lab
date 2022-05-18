4ca90b547da1d4e5ef0e1961f60b53350a37ba0f
Jackie <jye0325@gmail.com>
2022-05-18 16:33:52 -0400

    commit tree 92ad3acf007c6dccf97075fe673ddc69cafddd4e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, criteria) {
+    return drivers.filter(function(drivers) {
+        return drivers.toUpperCase() === criteria.toUpperCase();
+    })
+}
+
+function fuzzyMatch(drivers, criteria) {
+    return drivers.filter(function(drivers) {
+        return drivers.startsWith(criteria)
+    })
+
+}
+
+function matchName(drivers, criteria) {
+    return drivers.filter(function(drivers) {
+        return drivers.name === criteria
+    })
+
+}
